c621ab5e911f05b90c724ed1c1c836a3f90ca3c3
Dita Aisyiyah Larasati <63587595+ditaisyiyah@users.noreply.github.com>
2021-02-21 03:41:17 +0700

    Update Cacl4.java

--- 01-Calculations/Cacl4.java
+++ 01-Calculations/Cacl4.java
@@ -16,12 +16,14 @@ public class unit {
 		
 		System.out.print("Input any number to convert Gallon to Liter: ");
 		Scanner scG = new Scanner (System.in);
+		
 		double inputG = scG.nextDouble();
 		double resultL = inputG * GallonToLiter;
 		System.out.println("Your convertion result is: " + resultL);
 
 		System.out.print("\nInput any number to convert Liter to Gallon: ");
 		Scanner scL = new Scanner (System.in);
+		
 		double inputL = scL.nextDouble();
 		double resultG = inputL / GallonToLiter;
 		System.out.println("Your convertion result: " + resultG);
