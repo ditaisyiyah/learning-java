d4fd945cde7b2b9594f03682cc0d66aacaf9b0a8
Dita Aisyiyah Larasati <63587595+ditaisyiyah@users.noreply.github.com>
2021-02-21 03:33:53 +0700

    Add files via upload

---
+++ 01-Calculations/Cacl4.java
@@ -0,0 +1,39 @@
+package Dita.Calc;
+
+import java.util.Scanner;
+
+/*
+ * Case: conversion x-gallon to litter and otherwise
+ * Fact: 1 gallon equals to 3.785 litter
+ */
+
+public class unit {
+
+	private static final double GallonToLiter = 3.785;
+	private static final int coba = 3;
+	
+	public static void main (String [] args) {
+		
+		System.out.print("Input any number to convert Gallon to Liter: ");
+		Scanner scG = new Scanner (System.in);
+		double inputG = scG.nextDouble();
+		double resultL = inputG * GallonToLiter;
+		System.out.println("Your convertion result is: " + resultL);
+
+		System.out.print("\nInput any number to convert Liter to Gallon: ");
+		Scanner scL = new Scanner (System.in);
+		double inputL = scL.nextDouble();
+		double resultG = inputL / GallonToLiter;
+		System.out.println("Your convertion result: " + resultG);
+		
+		scG.close();
+		scL.close();
+		
+		double aboc = (double)coba;
+		System.out.println(aboc);
+	
+		int cucut = (int)GallonToLiter;
+		System.out.println(cucut);
+	}
+	
+}
---
+++ 01-Calculations/Calc2.java
@@ -0,0 +1,35 @@
+package Dita.Calc;
+/*
+ * Using built in method for calculation
+ */
+public class Calculate2 {
+	private double X = 1, Y = 1, Z = 1;
+
+	public Calculate2 (double valX, double valY, double valZ) {this.X = valX;	this.Y = valY;	this.Z = valZ;}
+	public Calculate2() {}
+
+	public void setX () {}
+	public double getX () {return X;}
+	public void setY () {}
+	public double getY () {return Y;}
+	public void setZ () {}
+	public double getZ () {return Z;}
+	
+	public double calcA() {double A = Math.sqrt(X-6)/4; 										return A;}
+	public double calcB() {double B = Math.pow(X, Y) - Math.pow(6, X);							return B;}
+	public double calcC() {double C = (4*Math.cos(Z/5)) - Math.sin(Math.pow(X, 6));				return C;}
+	public double calcD() {double D = Math.pow(X, 4) - Math.sqrt((6*X) - Math.pow(Y, 3));		return D;}
+	public double calcE() {double E = 1 / (Y - (1 / (X - Math.pow(2, Y))));						return E;}
+	public double calcF() {double F = 7*(Math.cos(Math.sqrt(5 - Math.sin(Math.sqrt(3*X - 4)))));return F;}
+
+	public static void main (String [] args) {
+		Calculate2 input = new Calculate2 (9, 7, 5);
+		
+		System.out.println(input.calcA());
+		System.out.println(input.calcB());
+		System.out.println(input.calcC());
+		System.out.println(input.calcD());
+		System.out.println(input.calcE());
+		System.out.println(input.calcF());
+	}
+}
