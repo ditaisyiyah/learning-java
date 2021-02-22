40eedcd166832a55890267d611ccd0c6f581f188
Dita Aisyiyah Larasati <63587595+ditaisyiyah@users.noreply.github.com>
2021-02-22 21:33:01 +0700

    Update Calc2.java

--- 01-Calculations/Calc2.java
+++ 01-Calculations/Calc2.java
@@ -1,17 +1,15 @@
 package Dita.Calc;
-
 /*
  * Using built in method for calculation
  */
-
 public class Calculate2 {
-	
 	private double X = 1, Y = 1, Z = 1;
 
 	public Calculate2 (double valX, double valY, double valZ) {
 		this.X = valX;	
 		this.Y = valY;	
 		this.Z = valZ;}
+	
 	public Calculate2() {}
 
 	public void setX () {}
@@ -21,18 +19,12 @@ public class Calculate2 {
 	public void setZ () {}
 	public double getZ () {return Z;}
 	
-	public double calcA() {double A = Math.sqrt(X-6)/4; 										
-			       return A;}
-	public double calcB() {double B = Math.pow(X, Y) - Math.pow(6, X);							
-			       return B;}
-	public double calcC() {double C = (4*Math.cos(Z/5)) - Math.sin(Math.pow(X, 6));				
-			       return C;}
-	public double calcD() {double D = Math.pow(X, 4) - Math.sqrt((6*X) - Math.pow(Y, 3));		
-			       return D;}
-	public double calcE() {double E = 1 / (Y - (1 / (X - Math.pow(2, Y))));						
-			       return E;}
-	public double calcF() {double F = 7*(Math.cos(Math.sqrt(5 - Math.sin(Math.sqrt(3*X - 4)))));
-			       return F;}
+	public double calcA() {double A = Math.sqrt(X-6)/4; 						return A;}
+	public double calcB() {double B = Math.pow(X, Y) - Math.pow(6, X);				return B;}
+	public double calcC() {double C = (4*Math.cos(Z/5)) - Math.sin(Math.pow(X, 6));			return C;}
+	public double calcD() {double D = Math.pow(X, 4) - Math.sqrt((6*X) - Math.pow(Y, 3));		return D;}
+	public double calcE() {double E = 1 / (Y - (1 / (X - Math.pow(2, Y))));				return E;}
+	public double calcF() {double F = 7*(Math.cos(Math.sqrt(5 - Math.sin(Math.sqrt(3*X - 4)))));	return F;}
 
 	public static void main (String [] args) {
 		Calculate2 input = new Calculate2 (9, 7, 5);
