3877c54ed0f78f265156b0703a3088a81719486b
Dita Aisyiyah Larasati <63587595+ditaisyiyah@users.noreply.github.com>
2021-02-22 21:33:41 +0700

    Update Calc3.java

--- 01-Calculations/Calc3.java
+++ 01-Calculations/Calc3.java
@@ -1,27 +1,22 @@
 package Dita.Calc;
-
 /*
  * Case: bus capacity = 45 people.
  * How much x-busses needed to carry y-people (input y value)
  */
-
 public class Calculate3 {
-	
 	private final static int Bus = 45;
 	private int People;
 	
 	public Calculate3 (int p) {
 		this.People = p;
 	}
+	
 	public Calculate3 () {}
 	
-	public int quotient () {
-		int res = People / Bus;  return res;}
-	public int remainder () {
-		int mod = People % Bus;  return mod;}
+	public int quotient () {int res = People / Bus;  	return res;}
+	public int remainder () {int mod = People % Bus;  	return mod;}
 	
 	public static void main (String [] args) {
-		
 		Calculate3 c1 = new Calculate3 (80);
 		
 		System.out.println(c1.quotient());
